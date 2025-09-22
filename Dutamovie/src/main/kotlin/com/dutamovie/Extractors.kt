a0c044594bc341ebf0bacb73ebdf6d3445aca14e
b4byhuey <60543438+b4byhuey@users.noreply.github.com>
2025-09-22 10:26:26 +0800

    Update Extractors.kt

--- Dutamovie/src/main/kotlin/com/dutamovie/Extractors.kt
+++ Dutamovie/src/main/kotlin/com/dutamovie/Extractors.kt
@@ -3,17 +3,17 @@ package com.dutamovie
 import com.lagradost.cloudstream3.extractors.JWPlayer
 
 
-class Embedfirex : JWPlayer() {
+class Embedpyrox : JWPlayer() {
     override var name = "embedpyrox"
     override var mainUrl = "https://embedpyrox.xyz"
 }
 
-class Embedfirex : JWPlayer() {
+class Helvid : JWPlayer() {
     override var name = "helvid"
     override var mainUrl = "https://helvid.net"
 }
 
-class Embedfirex : JWPlayer() {
+class P2pplay : JWPlayer() {
     override var name = "p2pplay"
     override var mainUrl = "https://pm21.p2pplay.pro"
 }
